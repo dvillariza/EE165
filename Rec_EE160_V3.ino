83fd0ae85929c62ce8d3217257c81af09a3f1196
CDishonoured109 <32019777+CDishonoured109@users.noreply.github.com>
2019-05-21 14:13:21 +0800

    Add files via upload

---
+++ Rec_EE160_V3.ino
@@ -0,0 +1,73 @@
+char message[10];
+char test;
+byte bet,teb;
+String con;
+int x;
+void setup() {
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(4,INPUT);
+  pinMode(5,INPUT);
+  pinMode(6,INPUT);
+  pinMode(7,INPUT);
+  pinMode(8,INPUT);
+  pinMode(9,INPUT);
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
+  pinMode(12,INPUT);
+  pinMode(13,INPUT);
+  bet = bet | B10000000;
+}
+
+void loop() {
+  if(digitalRead(2)==HIGH){ bet = bet|B00000001;}
+  else                    { bet = bet&B11111110;}     
+  if(digitalRead(3)==HIGH){ bet = bet|B00000010;}
+  else                    { bet = bet&B11111101;}   
+  if(digitalRead(4)==HIGH){ bet = bet|B00000100;}
+  else                    { bet = bet&B11111011;}   
+  if(digitalRead(5)==HIGH){ bet = bet|B00001000;}
+  else                    { bet = bet&B11110111;}   
+  if(digitalRead(6)==HIGH){ bet = bet|B00010000;}
+  else                    { bet = bet&B11101111;}   
+  if(digitalRead(7)==HIGH){ bet = bet|B00100000;}
+  else                    { bet = bet&B11011111;}   
+  if(digitalRead(8)==HIGH){ bet = bet|B01000000;}
+  else                    { bet = bet&B10111111;}   
+  if(digitalRead(9)==HIGH){ teb = teb|B00000001;}
+  else                    { teb = teb&B11111110;}  
+  if(digitalRead(10)==HIGH){ teb = teb|B00000010;}
+  else                    { teb = teb&B11111101;} 
+  if(digitalRead(11)==HIGH){ teb = teb|B00000100;}
+  else                    { teb = teb&B11111011;}   
+  if(digitalRead(12)==HIGH){ teb = teb|B00001000;}
+  else                    { teb = teb&B11110111;}   
+  if(digitalRead(13)==HIGH){ teb = teb|B00010000;}
+  else                    { teb = teb&B11101111;}            
+  //bet = B11100111;
+  //teb = B00001001;
+  /*
+  if ((teb & B10000000)== B10000000){
+    Serial.println("Good");
+  }
+  else{
+    Serial.println("Bad");
+  }
+  */
+
+  //
+  /*
+  Serial.print("First Byte");
+  Serial.println(bet,BIN);
+  Serial.print("Second Byte");
+  Serial.println(teb,BIN);
+  */
+  //
+  Serial.write(bet);
+  //delay(10); not needed
+  Serial.write(teb);
+  delay(200);
+  
+}
+
---
+++ Tra_EE160_V3.ino
@@ -0,0 +1,125 @@
+char message[13];
+char test;
+byte bet[2];
+String con;
+int x;
+/// not included
+struct DHT_11_H_T{
+  char ret[3];
+  float D_Temp;
+  float D_Hum;
+  float LM_Temp;
+} dht_sensor;
+//
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+  message[12] = '\0';
+}
+
+void loop() {
+  x = sizeof(dht_sensor);
+  Serial.println(x);
+  delay(10000);
+  if (Serial.available()){
+    bet[0] = Serial.read();
+    
+    if ((bet[0] & B10000000)== B10000000){
+      delay(10);
+      if (Serial.available()){
+        bet[1] = Serial.read();
+        //Serial.println(bet[0],BIN);
+        //Serial.println(bet[1],BIN);
+       }
+       
+      }
+    if ((bet[0] & B00000001)== B00000001){ 
+      message[0] = 'X';
+    }
+    else{
+      message[0] = 'O';
+    }
+    if ((bet[0] & B00000010)== B00000010){ 
+      message[1] = 'X';
+    }
+    else{
+      message[1] = 'O';
+    }
+    if ((bet[0] & B00000100)== B00000100){ 
+      message[2] = 'X';
+    }
+    else{
+      message[2] = 'O';
+    }
+    if ((bet[0] & B00001000)== B00001000){ 
+      message[3] = 'X';
+    }
+    else{
+      message[3] = 'O';
+    }
+    if ((bet[0] & B00010000)== B00010000){ 
+      message[4] = 'X';
+    }
+    else{
+      message[4] = 'O';
+    }
+    if ((bet[0] & B00100000)== B00100000){ 
+      message[5] = 'X';
+    }
+    else{
+      message[5] = 'O';
+    }
+    if ((bet[0] & B01000000)== B01000000){ 
+      message[6] = 'X';
+    }
+    else{
+      message[6] = 'O';
+    }
+    if ((bet[1] & B00000001)== B00000001){ 
+      message[7] = 'X';
+    }
+    else{
+      message[7] = 'O';
+    }
+    if ((bet[1] & B00000010)== B00000010){ 
+      message[8] = 'X';
+    }
+    else{
+      message[8] = 'O';
+    }
+    if ((bet[1] & B00000100)== B00000100){ 
+      message[9] = 'X';
+    }
+    else{
+      message[9] = 'O';
+    }
+    if ((bet[1] & B00001000)== B00001000){ 
+      message[10] = 'X';
+    }
+    else{
+      message[10] = 'O';
+    }
+    if ((bet[1] & B00010000)== B00010000){ 
+      message[11] = 'X';
+    }
+    else{
+      message[11] = 'O';
+    }
+    Serial.println(message);
+    }
+    /*
+    while (Serial.avaaailable()){
+      bet[x] = Serial.read();
+      x++;
+      delay(10);
+    }
+    Serial.println(sizeof(bet));
+    Serial.print("High");
+    Serial.prinssssstln(bet[0],BIN);
+    Serial.print("Low");
+    Serial.println(bet[1],BIN);
+  }
+  */
+  
+}
+
